69153e9bca00a974fc58bab0a18bb035cf8c3372
vescaname <vescaname298@gmail.com>
2025-05-28 14:36:45 -0600

    Set FPS

--- main.py
+++ main.py
@@ -4,13 +4,17 @@ from constants import *
 
 def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt=0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()  
-        
+        dt = clock.tick(60)     
+        dt = dt/1000
+
 if __name__ == "__main__":
     main()
 
